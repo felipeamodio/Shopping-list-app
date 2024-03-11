507abebdcefecaf38f57034bebd3f853b0b0b0fd
Felipe <felipeaamodio@gmail.com>
2024-03-11 18:15:18 -0300

    learning fundamentals of swift

---
+++ Lista de compras/Listas.swift
@@ -0,0 +1,43 @@
+//
+//  Listas.swift
+//  Lista de compras
+//
+//  Created by Felipe Alves Amodio on 11/03/24.
+//
+
+import Foundation
+import UIKit
+
+class Listas: UIViewController {
+    var v1 = 3
+    var v2 = 5
+    let v3 = 9
+    
+    override func viewDidLoad() {
+        print(v1)
+        NSLog("Resultado: %i", v1)
+        //        if(v1 < v2){
+        //            NSLog("V1 (%i) é menor do que V2 (%i)", v1, v2)
+        //        }else if(v3 > v2){
+        //            NSLog("V3 (%i) é maior do que V2 (%i)", v3, v2)
+        //        }else {
+        //            NSLog("Não sei o que colocar")
+        //        }
+        //    }
+        
+        let array = ["a", "b", "c"]
+        for i in array{
+            if(i == "b"){
+                print(i)
+            }
+        }
+        
+        for i in 0..<v2 {
+            print(i)
+        }
+        
+        for i in 0..<array.count {
+            print(i)
+        }
+    }
+}
